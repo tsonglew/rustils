a90ce857213220f260606bb722ee78407e5e6574
Tsong Lew <tsonglew@gmail.com>
2024-12-06 00:46:49 +0800

    test: update benchmarks for point3d_distance
    - Add single operation benchmarks
    - Add batch operation benchmarks
    - Compare Python and Rust implementations

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,38 +1,69 @@
+"""Benchmark tests for rustils"""
 import pytest
-from rustils import str_to_bool, py_str_to_bool
+from rustils import str_to_bool, py_str_to_bool, point3d_distance, py_point3d_distance
 
-TEST_CASES = [
-    "true", "false",
-    "True", "False",
-    "1", "0",
-    "yes", "no",
-    "Y", "N",
-    "on", "off"
+# Test data for string to bool conversion
+STR_TO_BOOL_CASES = [
+    "true",
+    "false",
+    "yes",
+    "no",
+    "1",
+    "0",
+    "on",
+    "off"
 ]
 
-def test_implementations_match():
-    """Ensure both implementations return the same results"""
-    for test_case in TEST_CASES:
-        assert str_to_bool(test_case) == py_str_to_bool(test_case)
+# Test data for 3D point distance calculation
+POINT3D_CASES = [
+    ((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)),  # Simple case
+    ((1.5, 2.7, 3.9), (4.2, 5.6, 6.8)),  # Floating point case
+    ((-1.0, -2.0, -3.0), (1.0, 2.0, 3.0)),  # Negative numbers
+    ((10.5, 20.5, 30.5), (40.5, 50.5, 60.5)),  # Larger numbers
+]
 
-def test_benchmark_rust(benchmark):
-    """Benchmark Rust implementation"""
-    def run_rust():
-        for test_case in TEST_CASES:
-            str_to_bool(test_case)
-    benchmark(run_rust)
+def test_benchmark_str_to_bool_rust(benchmark):
+    """Benchmark Rust implementation of str_to_bool"""
+    def run_conversion():
+        for case in STR_TO_BOOL_CASES:
+            str_to_bool(case)
+    benchmark(run_conversion)
 
-def test_benchmark_python(benchmark):
-    """Benchmark Python implementation"""
-    def run_python():
-        for test_case in TEST_CASES:
-            py_str_to_bool(test_case)
-    benchmark(run_python)
+def test_benchmark_str_to_bool_python(benchmark):
+    """Benchmark Python implementation of str_to_bool"""
+    def run_conversion():
+        for case in STR_TO_BOOL_CASES:
+            py_str_to_bool(case)
+    benchmark(run_conversion)
 
-def test_benchmark_single_rust(benchmark):
-    """Benchmark single call to Rust implementation"""
+def test_benchmark_point3d_distance_rust(benchmark):
+    """Benchmark Rust implementation of point3d_distance"""
+    def run_distance():
+        for p1, p2 in POINT3D_CASES:
+            point3d_distance(p1, p2)
+    benchmark(run_distance)
+
+def test_benchmark_point3d_distance_python(benchmark):
+    """Benchmark Python implementation of point3d_distance"""
+    def run_distance():
+        for p1, p2 in POINT3D_CASES:
+            py_point3d_distance(p1, p2)
+    benchmark(run_distance)
+
+def test_benchmark_str_to_bool_single_rust(benchmark):
+    """Benchmark single call to Rust str_to_bool"""
     benchmark(str_to_bool, "true")
 
-def test_benchmark_single_python(benchmark):
-    """Benchmark single call to Python implementation"""
+def test_benchmark_str_to_bool_single_python(benchmark):
+    """Benchmark single call to Python str_to_bool"""
     benchmark(py_str_to_bool, "true")
+
+def test_benchmark_point3d_single_rust(benchmark):
+    """Benchmark single call to Rust point3d_distance"""
+    p1, p2 = POINT3D_CASES[0]
+    benchmark(point3d_distance, p1, p2)
+
+def test_benchmark_point3d_single_python(benchmark):
+    """Benchmark single call to Python point3d_distance"""
+    p1, p2 = POINT3D_CASES[0]
+    benchmark(py_point3d_distance, p1, p2)
